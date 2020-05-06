12b36a52123ff9e1a054c8e37b3a883d8564befe
citizenz7 <citizenz7@protonmail.com>
2020-05-06 16:38:06 +0200

    integration php

---
+++ git.php
@@ -0,0 +1,10 @@
+<?php
+  incude_once 'header.php';
+?>
+
+<main>
+  Coucou !
+</main>
+
+<?php
+  include_once 'footer.php';
---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+  include_once 'header.php';
+?>
+
+  <div class="container">
+
+    <!-- Jumbotron -->
+    <div class="jumbotron mb-3 mt-3">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">Sur ce site, vous retrouverez toutes les informations utiles pour coder une page web.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+
+    <!-- cards -->
+    <div class="row">
+      <!-- Row 1 -->
+      <div class="col-sm-4 mb-4">
+        <div class="card">
+          <img class="card-img-top card-img-height" src="img/HTML5.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">HTML</h5>
+            <p class="card-text">
+              HTML5 (HyperText Markup Language 5) est la dernière révision majeure du HTML (format de données conçu pour représenter les pages web). Cette version a été finalisée le 28 octobre 2014. HTML5 spécifie deux syntaxes d'un modèle abstrait
+              défini en termes de DOM : HTML5 et XHTML5. Le langage comprend également une couche application avec de nombreuses API, ainsi qu'un algorithme afin de pouvoir traiter les documents à la syntaxe non conforme. Le travail a été repris par
+              le W3C en mars 2007 après avoir été lancé par le WHATWG. Les deux organisations travaillent en parallèle sur le même document afin de maintenir une version unique de la technologie. Le W3C clôt les ajouts de fonctionnalités le 22 mai
+              2011, annonçant une finalisation de la spécification en 2014, et encourage les développeurs Web à utiliser HTML 5 dès ce moment. Fin 2016, la version 5.1 est officiellement publiée et présente plusieurs nouveautés qui doivent faciliter
+              le travail des développeurs d'applications Web.
+            </p>
+            <div class="text-center"><a href="html.html" class="btn btn-primary">Read HTML</a></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <div class="card">
+          <img class="card-img-top card-img-height" src="img/CSS3.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">CSS</h5>
+            <p class="card-text">Les feuilles de style en cascade, généralement appelées CSS de l'anglais Cascading Style Sheets, forment un langage informatique qui décrit la présentation des documents HTML et XML. Les standards définissant CSS sont
+              publiés par le World Wide Web Consortium (W3C). Introduit au milieu des années 1990, CSS devient couramment utilisé dans la conception de sites web et bien pris en charge par les navigateurs web dans les années 2000.</p>
+            <div class="text-center"><a href="css.html" class="btn btn-primary">Read CSS</a></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <div class="card">
+          <img class="card-img-top card-img-height" src="img/terminal.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Command line</h5>
+            <p class="card-text">Un terminal est un programme qui émule une console dans une interface graphique, il permet de lancer des commandes...</p>
+            <div class="text-center"><a href="console.html" class="btn btn-primary">Read Command line</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <!-- Row 2 -->
+      <div class="col-sm-4 mb-4">
+        <div class="card">
+          <img class="card-img-top card-img-height" src="img/git.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Git & Github</h5>
+            <p class="card-text">Git est un logiciel de gestion de versions décentralisé. C'est un logiciel libre créé par Linus Torvalds, auteur du noyau Linux, et distribué selon les termes de la licence publique générale GNU version 2. En 2016, il
+              s’agit du logiciel de gestion de versions le plus populaire qui est utilisé par plus de douze millions de personnes.</p>
+            <div class="text-center"><a href="git.html" class="btn btn-primary">Read Git & Github</a></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <div class="card">
+          <img class="card-img-top card-img-height" src="img/atom.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Atom</h5>
+            <p class="card-text">Atom est un éditeur de texte libre pour macOS, GNU/Linux et Windows développé par GitHub. Il supporte des plug-ins écrits en Node.js et implémente Git Control. La plupart des extensions sont sous licence libre et sont
+              maintenues par la communauté. Atom est basé sur Chromium et Electron et est écrit en CoffeeScript10. Il est aussi utilisé en tant qu’environnement de développement (EDI).</p>
+            <div class="text-center"><a href="atom.html" class="btn btn-primary">Read Atom</a></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <div class="card">
+          <img class="card-img-top card-img-height" src="img/linux.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Gnu/Linux</h5>
+            <p class="card-text">Le débat « Linux ou GNU/Linux » est une controverse divisant les partisans du logiciel libre sur le nom à donner aux systèmes d'exploitation fondés sur le système GNU et un noyau Linux. Les plus nombreux (avec le
+              grand public) l'appellent simplement « Linux », les autres (peut-être plus proches du projet GNU) l'appellent « GNU/Linux ».</p>
+            <div class="text-center"><a href="linux.html" class="btn btn-primary">Read Gnu/Linux</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<?php
+  include_once 'footer.php';
